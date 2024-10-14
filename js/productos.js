5390b75729a26416fd4f2fe1f8394eb04e3b6336
Rodrigo Cortez <rodriiccortez@gmail.com>
2024-10-14 19:56:00 -0300

    agregado

--- js/productos.js
+++ js/productos.js
@@ -32,7 +32,7 @@ fetch("../db/data.json")
 
 function addToCartButton() {
   const addButton = document.querySelectorAll(".productoAgregar");
-  addButton.forEach((button) => {
+    addButton.forEach((button) => {
     button.onclick = (e) => {
       const productId = e.currentTarget.id;
       const quantityInput = document.getElementById(`quantity-${productId}`);
@@ -54,7 +54,21 @@ function addToCartButton() {
 
         localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
              } 
-       
+             Toastify({
+              text: "Producto agregado",
+              duration: 2000,
+              destination: "#",
+              newWindow: false,
+              close: false,
+              gravity: "top", // `top` or `bottom`
+              position: "right", // `left`, `center` or `right`
+              stopOnFocus: false, // Prevents dismissing of toast on hover
+              style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+              },
+              onClick: function(){} // Callback after click
+            }).showToast();
+            
       
     };
   });
